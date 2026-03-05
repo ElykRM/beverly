e61cdc23df2373f2fd6f59a7b7c420b181077c55
ElykRM <krpmab@gmail.com>
2026-03-05 15:23:43 +0800

    Delete pages/includes directory

--- pages/includes/footer.php
+++
@@ -1,8 +0,0 @@
-    </main>
-
-    <footer class="bg-gray-800 text-white text-center py-6 mt-auto">
-        <p>© <?php echo date('Y'); ?> Beverly Homes Phase 1 HOA. All rights reserved.</p>
-    </footer>
-
-</body>
-</html>
